fc3f6e1a07c264c4d1c3ecd7fd755f215b66a3b0
Ivonnefr <ivflores2017@inf.udec.cl>
2023-04-18 14:59:18 -0400

    v1

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from website import create_app
+from flask import render_template, request
 
 app = create_app()
 
+@app.route('/',methods = ['GET','POST'])
+def home():
+    if request.method == 'POST':
+        archivo = request.form['archivo_java']
+    return render_template('home.html')
+
 if __name__ ==  '__main__':
     app.run(debug=True)
+
+
 
