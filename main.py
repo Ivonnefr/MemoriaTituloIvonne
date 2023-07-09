831402b2549a67db8bd63b2fbab14eae6135f0e6
Ivonnefr <ivflores2017@inf.udec.cl>
2023-07-08 23:23:14 -0400

    agregue varias rutas

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
 import subprocess
+from click import DateTime
 from flask import Flask, render_template, request, url_for, redirect, jsonify, session, flash
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm 
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy 
 from wtforms import FileField, SubmitField, PasswordField,StringField
@@ -77,8 +79,97 @@ def register():
         return redirect(url_for('login'))
 
     return render_template('register.html')
-#Ruta para ver el listado de ejercicios, verificando que el usuario este logeado
-@app.route('/ejercicios', methods=['GET',"POST"])
+
+
+#Ruta para el docente:
+@app.route('/vistaDocente', methods=['GET', 'POST'])
+def rutaDocente():
+    #Muestra el listado de todos los ejercicios y series, botones para agregar ejercicio y serie.
+    # Obtiene todas las series
+    series = Serie.query.all()
+    
+    # Crea un diccionario donde las llaves son los id de las series y los valores son listas de ejercicios
+    ejercicios_por_serie = {serie.id: [] for serie in series}
+    
+    # Obtiene todos los ejercicios
+    ejercicios = Ejercicio.query.all()
+    
+    # Agrega los ejercicios a las listas correspondientes en el diccionario
+    for ejercicio in ejercicios:
+        ejercicios_por_serie[ejercicio.id_serie].append(ejercicio)
+    
+    #Si se presiona el boton de agregar serie, se muestra un formulario para agregar una serie.
+    #Si se presiona el boton de agregar ejercicio, se muestra un formulario para agregar un ejercicio.
+    
+    #Si se presiona el boton de editar serie, se muestra un formulario para editar una serie.
+    #Si se presiona el boton de editar ejercicio, se muestra un formulario para editar un ejercicio.
+    #Si se presiona el boton de eliminar serie, se muestra un formulario para eliminar una serie.
+    #Si se presiona el boton de eliminar ejercicio, se muestra un formulario para eliminar un ejercicio.
+    return render_template('vistaDocente.html',series=series, ejercicios_por_serie=ejercicios_por_serie)
+
+#Ruta para agregar una serie
+@app.route('/vistaDocente/agregarSerie', methods=['GET', 'POST'])
+def agregar_serie():
+    # Muestra un formulario para agregar una serie
+    if request.method == 'POST':
+        nombreSerie = request.form.get('nombre')
+        fecha_str = request.form.get('fecha')
+        # Convertir la fecha a un objeto date
+        fecha = datetime.date.fromisoformat(fecha_str)
+
+        # Validar los datos ingresados
+        if not nombreSerie or not fecha:
+            flash('El nombre de la serie y la fecha de creacion son requeridos.')
+            return redirect(url_for('vistaDocente.agregar_serie'))
+
+        # Verificar si el nombre de la serie ya existe en la base de datos
+        serie_existente = Serie.query.filter_by(nombre=nombreSerie).first()
+        if serie_existente:
+            flash('El nombre de la serie ya existe.')
+            return redirect(url_for('vistaDocente.agregar_serie'))  # Cambiar esto
+
+        # Crear una nueva instancia de la Serie
+        nueva_serie = Serie(nombre=nombreSerie, fecha=fecha)
+
+        # Guardar la nueva serie en la base de datos
+        db.session.add(nueva_serie)
+        db.session.commit()
+
+        # Redireccionar a '/vistaDocente' después de agregar la serie exitosamente
+        return redirect(url_for('rutaDocente'))
+    return render_template('agregarSerie.html')
+
+
+
+#Ruta para agregar un ejercicio a una serie que ya existe
+@app.route('/vistaDocente/agregarEjercicio', methods=['GET', 'POST'])
+def agregar_ejercicio():
+    #Recibe un formulario con los datos del ejercicio.
+
+    return render_template('agregarEjercicio.html')
+
+# Ruta para editar una serie
+@app.route('/vistaDocente/editarSerie', methods=['GET', 'POST'])
+def editar_serie():
+    return render_template('editarSerie.html')
+
+@app.route('/ejercicios', methods=['GET', 'POST'])
+def listar_ejercicios():
+    # Obtiene todas las series
+    series = Serie.query.all()
+    
+    # Crea un diccionario donde las llaves son los id de las series y los valores son listas de ejercicios
+    ejercicios_por_serie = {serie.id: [] for serie in series}
+    
+    # Obtiene todos los ejercicios
+    ejercicios = Ejercicio.query.all()
+    
+    # Agrega los ejercicios a las listas correspondientes en el diccionario
+    for ejercicio in ejercicios:
+        ejercicios_por_serie[ejercicio.id_serie].append(ejercicio)
+    
+    # Devuelve la plantilla con el diccionario de ejercicios por serie
+    return render_template('ejercicios.html', series=series, ejercicios_por_serie=ejercicios_por_serie)
 
 
 
@@ -108,22 +199,25 @@ def upload_file():
     
     return render_template('upload_file.html', form=form)
 
+
+
+
 #Ruta siguiente despues de subir el archivo, donde se muestran los resultados de aplicar los test unitarios
 @app.route('/upload_file/pregunta', methods=["POST"])
 def pregunta():
 
     return render_template('pregunta.html')
 
-#Ruta para la vista del profesor
-@app.route('/profesor', methods=["GET","POST"])
-def profesor():
-    return render_template('profesor.html')
+
 
 
 #Funcion para ejecutar el script 404
 def pagina_no_encontrada(error):
     return render_template('404.html'), 404
     #return redirect(url_for('index')) #te devuelve a esa página
+
+
+
 
 #Ruta para ejecutar el script
 if __name__ == '__main__':
