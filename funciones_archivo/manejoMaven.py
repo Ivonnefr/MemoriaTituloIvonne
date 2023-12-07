4cb68b352b0f3ae1c097343d7d8d20da1a4bc3c2
Ivonnefr <ivflores2017@inf.udec.cl>
2023-12-07 00:31:51 -0300

    cambios generales en bd y agrego archivos

--- funciones_archivo/manejoMaven.py
+++ funciones_archivo/manejoMaven.py
@@ -8,7 +8,7 @@ def ejecutarTestUnitario(rutaEjercicioEstudiante):
     patron_success = re.compile(r'BUILD SUCCESS', re.DOTALL)
     patron_failures = re.compile(r'Failures:(.+?)BUILD FAILURE', re.DOTALL)
     patron_comp = re.compile(r'COMPILATION ERROR(.+?)BUILD FAILURE', re.DOTALL)
-
+    patron_compile = re.compile(r'BUILD FAILURE(.+?)Help 1', re.DOTALL)
     coincidencias=patron_success.search(resultado.stdout)
     if coincidencias:
         return 'BUILD SUCCESS'
@@ -25,14 +25,15 @@ def ejecutarTestUnitario(rutaEjercicioEstudiante):
                     errores.append(linea_limpia)
             return errores
         else:
-            coincidencias = patron_comp.search(resultado.stdout)
-            if coincidencias:
-                lineas_test = coincidencias.group(1).split('\n')
-                converter = Ansi2HTMLConverter()
-                errores = []
-                for linea in lineas_test:
-                    if 'ERROR' in linea:
-                        # Filtra los códigos ANSI y agrega la línea limpia
-                        linea_limpia = converter.convert(linea, full=False)
-                        errores.append(linea_limpia)
+            if patron_comp.search(resultado.stdout):
+                coincidencias = patron_compile.search(resultado.stdout)
+                if coincidencias:
+                    lineas_test = coincidencias.group(1).split('\n')
+                    converter = Ansi2HTMLConverter()
+                    errores = []
+                    for linea in lineas_test:
+                        if 'ERROR' in linea:
+                            # Filtra los códigos ANSI y agrega la línea limpia
+                            linea_limpia = converter.convert(linea, full=False)
+                            errores.append(linea_limpia)
             return errores
